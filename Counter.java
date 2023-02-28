e1762391fa60699cd6d8d8c10c1c0dbdf442f339
Henjie Carmelotes <101442508+HenjieCarmelotes@users.noreply.github.com>
2023-02-28 11:03:47 +0700

    Implements Generic counter

--- Counter.java
+++ Counter.java
@@ -1,25 +1,36 @@
-package oop;
+package genericprogramming;
 
-/**
- * An object of this class represents a counter that counts up from zero.
- */
+import java.util.Collection;
+import java.util.function.Predicate;
 
 public class Counter {
 	
-	private int value = 0;  // Current value of the counter.
+	//implements countif in 2 implementation: For loop and Stream API
+	
+	//*** For Loop implementation for countIF.
+	public static <T> int countIf( Collection<T> items, Predicate<T> pred )  {
+	    int count = 0;
+	    for ( T x : items ) {
+	        if ( pred.test(x) )
+	            count++;
+	    }
+	    return count;
+	}
+	
+	//*** Stream API Implementation of countIf
+	
+	
+	public static <T> int countIfs( Collection<T> items, Predicate<T> pred ) {
+	    long ct = items.parallelStream()
+	                   .filter(pred)
+	                   .count();
+	         // .count() returns a long, but I want countIf to return an int
+	    return (int)ct; 
+	}
+	
+	
+	
+	
+	
 
-	   /**
-	    * Add one to the value of the counter.
-	    */
-	public void increment() {  
-	    value++;
-	   }
-
-	   /**
-	    * Returns the current value of the counter.
-	    */
-	public int getValue() {    
-	    return value;
-	   }
-
-} //end of class Counter
+} //end class Counter
